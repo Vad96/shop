7fe4d1c23f06059257c5e932b9cbccecbdf05ee8
Vad96 <vadim96dk@gmail.com>
2020-11-05 14:30:39 +0200

    added ant design

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
+import Header from './components/nav/Header';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </>
   )
 };
 
