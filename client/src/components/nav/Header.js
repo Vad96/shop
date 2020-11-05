2a36d8acad322b78f2fe0c3f6e8abffb292746fa
Vad96 <vadim96dk@gmail.com>
2020-11-05 15:21:37 +0200

    added link

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
 import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 const { SubMenu, Item } = Menu; //Menu.Submenu
 
@@ -15,15 +16,15 @@ const Header = () => {
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
       <Item key="home" icon={<AppstoreOutlined />}>
-        Home
+        <Link to="/">Home</Link>
       </Item>
 
       <Item key="register" icon={<UserAddOutlined />} className="float-right">
-        Register
+        <Link to="/register">Register</Link>
       </Item>
 
       <Item key="login" icon={<UserOutlined />} className="float-right">
-        Login
+        <Link to="/login">Login</Link>
       </Item>
 
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
