ef334ea818f00327a01cd2ab3b5afbc1381d8abb
Vad96 <vadim96dk@gmail.com>
2020-11-06 18:04:06 +0200

    user in redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,7 +9,31 @@ import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  // check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult()
+        console.log(user)
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        })
+      }
+    });
+    // clear
+    return () => unsubscribe()
+  }, [])
+
   return (
     <>
       <Header />
