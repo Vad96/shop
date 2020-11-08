62f9dd4c74639d4ab143b03f7a8cc8ac4e614341
Vad96 <vadim96dk@gmail.com>
2020-11-08 18:50:35 +0200

    category routes

--- server/models/category.js
+++ server/models/category.js
@@ -5,7 +5,7 @@ const categorySchema = new mongoose.Schema({
     type: String,
     trim: true,
     required: 'name is required',
-    minlength: [3, 'too short'],
+    minlength: [2, 'too short'],
     maxlength: [32, 'too long']
   },
   slug: {
---
+++ server/routes/category.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller
+const { create, read, update, remove, list } = require("../controllers/category");
+
+router.post("/category", authCheck, authCheck, adminCheck, create);
+router.get("/categories", list);
+router.get("/category:slug", authCheck, authCheck, adminCheck, read);
+router.put("/category/:slug", authCheck, authCheck, adminCheck, update);
+router.delete("/category", authCheck, authCheck, adminCheck, remove);
+
+module.exports = router;
